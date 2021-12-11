19fa8e486e109c3d8797822ab95115b52977c6a4
Andrew <diver.vlz@gmail.com>
2021-12-12 01:34:50 +0300

    create model 'Vacancy'

--- crud/models.py
+++ crud/models.py
@@ -5,5 +5,9 @@ class Posts(models.Model):
     post_title = models.CharField(max_length=300, unique=True)
     time = models.DateTimeField(auto_now_add=True)
 
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+
     def __str__(self):
         return self.post_title
