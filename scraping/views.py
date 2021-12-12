7ec2afa2f50348a6109a4b97c7122fbf92a83ab5
Andrew <diver.vlz@gmail.com>
2021-12-12 20:06:22 +0300

    html templates

--- scraping/views.py
+++ scraping/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import City, Language
+from .models import City, Language, Vacancy
 
 
 def main(request):
-    cities = City.objects.all()
+    cities_list = City.objects.all()
+    lang_list = Language.objects.all()
+    job_list = Vacancy.objects.all()
     context = {
         'title': 'It works',
-        'cities_list': cities,
+        'cities_list': cities_list,
+        'lang_list': lang_list,
+        'job_list': job_list,
     }
     return render(request, 'scrap.html', context)
