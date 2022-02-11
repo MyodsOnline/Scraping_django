596695fcd0e3ed5f92bd4e33cf7dfe5860e39d15
Andrew Smith <diver.vlz@gmail.com>
2022-02-11 04:28:24 +0300

    choices.field for crud

--- crud/models.py
+++ crud/models.py
@@ -2,9 +2,17 @@ from django.db import models
 
 
 class Posts(models.Model):
+    AVAILABLE_EQUIPMENT_MODES = [
+        ('AP', 'аварийный ремонт'),
+        ('TP', 'текущий ремонт'),
+        ('XP', 'холодный резерв'),
+        ('on', 'в работе'),
+    ]
+
     post_title = models.CharField(max_length=300, unique=True)
     time = models.DateTimeField(auto_now_add=True)
     is_posted = models.BooleanField(default=False)
+    equipment_mode = models.CharField(max_length=3, choices=AVAILABLE_EQUIPMENT_MODES, default='on')
 
     class Meta:
         verbose_name = 'Post'
