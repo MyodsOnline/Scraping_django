2d6fa4e2925622ecf23689800a320fbeb0e8456a
Andrew Smith <diver.vlz@gmail.com>
2022-06-28 17:22:34 +0300

    boss_says update

--- crud/urls.py
+++ crud/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import Posts_list, Post_Update
+from .views import Posts_list, Post_Update, json_file
 
 urlpatterns = [
     path('', Posts_list.as_view(), name='posts-list'),
     path('post/<slug:pk>', Post_Update.as_view(), name='post-update'),
+    path('json/', json_file, name='json_out'),
 ]
--- crud/views.py
+++ crud/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
+import os
+import json
 
 from .models import Posts
+
+SOURCE_DIR = os.path.dirname(__file__)
+source_file = os.path.join(SOURCE_DIR, 'fixtures/Export_DataFrame.json')
 
 
 class Posts_list(ListView):
@@ -16,3 +21,12 @@ class Post_Update(UpdateView):
     fields = ['post_title', 'is_posted', 'equipment_mode']
     template_name = 'crud_update.html'
     success_url = '/crud/'
+
+
+def json_file(request):
+    json_data = json.load(open(source_file, encoding='utf-8'))
+    context = {
+        'json_data': json_data,
+    }
+
+    return render(request, 'json_out.html', context)
